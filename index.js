330474393adfd97bb76e38979d5aff3c7fed8b24
Danny0177 <dstreet710@gmail.com>
2026-07-07 14:54:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const mineflayer = require("mineflayer");
+const settings = require("./settings.json");
 
+function createBot() {
+
+    const bot = mineflayer.createBot({
+        host: settings.server.host,
+        port: settings.server.port,
+        username: settings.account.username,
+        version: settings.server.version
+    });
+
+
+    bot.once("spawn", () => {
+        console.log("Bot connected!");
+
+        if (settings.loginCommand) {
+            setTimeout(() => {
+                bot.chat(settings.loginCommand);
+                console.log("Login command sent");
+            }, 2000);
+        }
+    });
+
+
+    bot.on("end", () => {
+        console.log("Disconnected. Reconnecting...");
+        setTimeout(createBot, settings.reconnectDelay);
+    });
+
+
+    bot.on("error", err => {
+        console.log("Bot error:", err.message);
+    });
+}
+
+createBot();
